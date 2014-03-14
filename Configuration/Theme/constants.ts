67a427d60cafab01353acd140551a5731d897894
Kay Strobach <github@kay-strobach.de>
2014-03-14 08:39:43 +0100

    fix path

--- Configuration/Theme/constants.ts
+++ Configuration/Theme/constants.ts
@@ -1,3 +1,3 @@
 plugin.tx_theme_bootstrap {
-	bootstrapTheme = {$plugin.tx_themes.relPath}Resources/Public/Contrib/Bootstrap/css/bootstrap.min.css
+	bootstrapTheme = {$plugin.tx_themes.resourcesPublicPath}Contrib/Bootstrap/css/bootstrap.min.css
 }
