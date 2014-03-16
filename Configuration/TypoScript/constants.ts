dbeb0808e51a40bd8e74d98cca05a0d1b9732306
Thomas Deuling <info@coding.ms>
2014-03-16 13:15:50 +0100

    Fluid-Template and base TypoScript

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,3 +1,5 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes_gridelements/Configuration/TypoScript/constants.ts">
+
 plugin.tx_theme_bootstrap {
 	bootstrapTheme = {$plugin.tx_themes.resourcesPublicPath}Contrib/Bootstrap/css/bootstrap.min.css
 
@@ -5,9 +7,9 @@ plugin.tx_theme_bootstrap {
         siteName = Bootstrap Theme
         colors {
             # cat=site colors; type=color; label= color: a color picker
-            link      = #FF0000
-            primary   = #FF0000
-            secondary = #FF0000
+            #link      = #FF0000
+            #primary   = #FF0000
+            #secondary = #FF0000
 
             # cat=site colors; type=color; label= Success color
             information = #eaf7ff
