4fa2bcf6b89b9e403e62233abdd6426e3a947a5a
Thomas Deuling <info@coding.ms>
2014-04-30 13:44:53 +0200

    backendlayouts and constants

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,9 +1,9 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:themes_gridelements/Configuration/TypoScript/constants.ts">
 
-plugin.tx_theme_bootstrap {
-	bootstrapTheme = {$plugin.tx_themes.resourcesPublicPath}Contrib/Bootstrap/css/bootstrap.min.css
+themes {
+	bootstrapTheme = {$themes.resourcesPublicPath}Contrib/Bootstrap/css/bootstrap.min.css
 
-    confirguration {
+    configuration {
         siteName = Bootstrap Theme
         colors {
             # cat=site colors; type=color; label= color: a color picker
--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,5 +1,5 @@
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:themes_gridelements/Configuration/TypoScript/">
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:theme_bootstrap/Configuration/TypoScript/Library/">
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:themes_gridelements/Configuration/TypoScript/" extension="setupts">
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:theme_bootstrap/Configuration/TypoScript/Library/" extension="setupts">
 
 page {
 	typeNum = 0
@@ -10,6 +10,7 @@ page {
 	includeCSS {
         file1 = EXT:theme_bootstrap/Resources/Public/Contrib/bootstrap-3.1.1/less/bootstrap.less
 		file2 = EXT:theme_bootstrap/Resources/Public/Stylesheets/main.css
+		file3 = EXT:theme_bootstrap/Resources/Private/Dyncss/import.less
 	}
 	includeJS {
 		file1 = EXT:theme_bootstrap/Resources/Public/Contrib/Bootstrap/js/bootstrap.js
