521b0b6c02d3d35fcbf98cd6fe6d51770bc3e47c
Jo Hasenau <jo.hasenau@external.telekom.de>
2015-04-18 17:53:27 +0200

    [TASK] Moved TCA to overrides and added language labels

--- ext_tables.php
+++ ext_tables.php
@@ -7,7 +7,3 @@
 //if(!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('themes')) {
 //    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', $_EXTKEY . ': theme');
 //}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'subheader;LLL:EXT:theme_bootstrap/Resources/Private/Language/locallang.xlf:content.pricing_table_listheader', 'bullets', 'replace:header_link');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'altText;LLL:EXT:theme_bootstrap/Resources/Private/Language/locallang.xlf:content.pricing_table_linktext,header_link', 'bullets', 'after:bodytext');
-
