896bce4614f209182a241540dea36b5ac2a4f1e3
Kay Strobach <github@kay-strobach.de>
2016-11-14 16:24:04 +0000

    Applied fixes from StyleCI

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,50 +1,48 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "templavoila_framework".
-#
-# Auto generated 25-08-2011 21:37
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+//#######################################################################
+// Extension Manager/Repository config file for ext "templavoila_framework".
+//
+// Auto generated 25-08-2011 21:37
+//
+// Manual updates:
+// Only the data in the array - everything else is removed by next
+// writing. "version" and "dependencies" must not be touched!
+//#######################################################################
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Bootstrap Theme',
-	'description' => 'Theme to use Twitter Bootstrap',
-	'category' => 'templates',
-	'shy' => 0,
-	'version' => '7.0.1',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Themes-Team (Kay Strobach, Jo Hasenau, Thomas Deuling)',
-	'author_email' => 'team@typo3-themes.org',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3'  => '7.6.0-7.6.99',
-			'themes_gridelements' => '7.0.0-7.99.99',
-			'dyncss_less' => '0.7.0-0.7.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-			'dyncss_scss' => '0.7.1-0.7.99',
-		),
-	),
-	'_md5_values_when_last_written' => 'a:0:{}',
-);
-
-?>
+$EM_CONF[$_EXTKEY] = [
+    'title'              => 'Bootstrap Theme',
+    'description'        => 'Theme to use Twitter Bootstrap',
+    'category'           => 'templates',
+    'shy'                => 0,
+    'version'            => '7.0.1',
+    'dependencies'       => '',
+    'conflicts'          => '',
+    'priority'           => '',
+    'loadOrder'          => '',
+    'module'             => '',
+    'state'              => 'beta',
+    'uploadfolder'       => 0,
+    'createDirs'         => '',
+    'modify_tables'      => '',
+    'clearcacheonload'   => 0,
+    'lockType'           => '',
+    'author'             => 'Themes-Team (Kay Strobach, Jo Hasenau, Thomas Deuling)',
+    'author_email'       => 'team@typo3-themes.org',
+    'author_company'     => '',
+    'CGLcompliance'      => '',
+    'CGLcompliance_note' => '',
+    'constraints'        => [
+        'depends' => [
+            'typo3'               => '7.6.0-7.6.99',
+            'themes_gridelements' => '7.0.0-7.99.99',
+            'dyncss_less'         => '0.7.0-0.7.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+            'dyncss_scss' => '0.7.1-0.7.99',
+        ],
+    ],
+    '_md5_values_when_last_written' => 'a:0:{}',
+];
